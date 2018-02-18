e0c86d1c5660a6a7bf3844c2b48a9ebd38ef14de
cengiz-dev <33718334+cengiz-dev@users.noreply.github.com>
2018-02-18 15:49:43 +0300

    Create environment.firebase.ts for running demo on StackBlitz.

---
+++ src/environments/environment.firebase.ts
@@ -0,0 +1,11 @@
+// TODO: Replace this information with that for your own Firebase app.
+// This info has been placed here to be able to run the demo via StackBlitz.
+
+export const firebaseEnvironment = {
+  apiKey: "AIzaSyCZKUZIiYNTEzhON95xBhgF3hors7yUyEU",
+  authDomain: "scrumbase-demo.firebaseapp.com",
+  databaseURL: "https://scrumbase-demo.firebaseio.com",
+  projectId: "scrumbase-demo",
+  storageBucket: "scrumbase-demo.appspot.com",
+  messagingSenderId: "20296497620"
+};
