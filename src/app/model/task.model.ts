cf5553241103a797d9294a6a22f5de5eaac6ea5d
Cengiz <cengiz.oner.d@gmail.com>
2018-03-22 17:10:06 +0300

    Update task model.

--- src/app/model/task.model.ts
+++ src/app/model/task.model.ts
@@ -3,20 +3,35 @@ import { TaskStatus } from "./task-status.model";
 import { TaskPriority } from "./task-priority.model";
 import { User } from "./user.model";
 
-export class Task {
+export class TaskSummary {
     public description: string;
-    public points: number;
-    public priority: TaskPriority;
-    public status: TaskStatus;
-    public subTasks: Task[];
+    public type: TaskType;
     public createdOn: any;
     public createdBy: User;
     public lastUpdatedOn: any;
     public lastUpdatedBy: User;
 
+    constructor(public title: string) { }
+}
+
+export class Task extends TaskSummary {
+    public points: number;
+    public priority: TaskPriority;
+    public status: TaskStatus;
+
     // TODO:
     // assignedTo
+    // subTasks ?
     // sprints ?
 
-    constructor(public title: string) { }
+    constructor(title: string) {
+        super(title);
+    }
+}
+
+export class TaskRef extends Task {
+    public static COLLECTION_NAME = 'tasks';
+    constructor(public id: string, public title: string) {
+        super(title);
+    }
 }
