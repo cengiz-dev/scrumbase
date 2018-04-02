8e718a4d42cfb0eea9c92bc64221587c72c9a476
Cengiz <cengiz.oner.d@gmail.com>
2018-04-02 14:30:16 +0300

    Clean up firebase data service test file.

--- src/app/data/firebase-data.service.spec.ts
+++ src/app/data/firebase-data.service.spec.ts
@@ -1,14 +1,6 @@
 import { inject, TestBed } from '@angular/core/testing';
 
-import { Category } from '../products/category.model';
-import { Product } from '../products/product.model';
 import { FirebaseDataService } from './firebase-data.service';
-
-const mockProducts: Product[] = [
-    new Product('Elma', Category.FOOD, 'Organik elma', 'Image Path'),
-    new Product('Portakal', Category.FOOD, 'Organik portakal', 'Image Path'),
-    new Product('Bebek Body', Category.CLOTHING, 'Organik pamuklu bebek body', 'Image Path')
-  ];
 
 describe('FirebaseDataService', () => {
   beforeEach(() => {
